156af138959a0c089d877e83cb6c22c1c05ac7d6
DmitriYam <dmitriyam.rex1738@gmail.com>
2024-12-02 01:03:13 +0800

    delete stuff

--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/dmitri_yam/aoc_2024/day1/target/debug/day1: /home/dmitri_yam/aoc_2024/day1/src/main.rs
--- day1/target/debug/deps/day1-b9b26dcf7b30796b.d
+++
@@ -1,5 +0,0 @@
-/home/dmitri_yam/aoc_2024/day1/target/debug/deps/libday1-b9b26dcf7b30796b.rmeta: src/main.rs
-
-/home/dmitri_yam/aoc_2024/day1/target/debug/deps/day1-b9b26dcf7b30796b.d: src/main.rs
-
-src/main.rs:
